1207362fbefb94633304c2216199f764110f8723
mythterncf <127208978+keijoss@users.noreply.github.com>
2024-05-16 21:31:19 +0800

    here na pi huhuh ang error and bugs

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, SafeAreaView, Platform, StatusBar, ScrollView, Image} from 'react-native';
 
 export default function AboutScreen() {
   return (
-    <View>
-      <Text>About Screen</Text>
-    </View>
+    <>
+      <ScrollView>
+        <SafeAreaView styles={styles.AndroidSafeArea}>
+          <View style={styles.header}>
+            <Image source={require('../assets/images/ncf-logo.png')} />
+            <Text style={styles.pageTitle}>About Me</Text>
+            <Text style={{fontSize: 12}}>Happy Anniversary!</Text>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    margin: 1,
+  },
+  header: {
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 15,
+    justifyContent: "space-between",
+    margin: 10,
+  },
+  headerImage: {
+    width: 100,
+    height: 100,
+  },
+  pageTitle: {
+    fontFamily: "sans-serif",
+    color: "black",
+    fontSize: 30,
+  },
+});
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,8 +7,8 @@ const HomeScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Image source={require('../assets/images/ncf-logo.png')} style={{ width: 200, height: 200, borderRadius: 100 }} />
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Your Name</Text>
-      <Text style={{ fontSize: 16 }}>A short bio about yourself</Text>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Mj Estepanie Palo</Text>
+      <Text style={{ fontSize: 16 }}>Hello! I am Mj a 3rd year student of Naga College Foundation </Text>
 
       <Navbar />
     </View>
